07df7eb54ba34da1260da808cc952d9e19ff5dae
Srilaxmi Nowduri <nowduribtv@gmail.com>
2024-10-23 23:24:43 -0500

    File Upload

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const mongoose = require('mongoose');
+const uploadRouter = require('./routes/uploadRouter');
+
 
 // const session = require('express-session');
 // const FileStore = require('session-file-store')(session);
@@ -58,6 +60,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
