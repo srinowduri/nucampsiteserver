414bb1b2c7d6e083e5b21c1bfbdff116c7c68775
Srilaxmi Nowduri <nowduribtv@gmail.com>
2024-10-28 16:35:57 -0500

    node.js nucamp week 4 assignment

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const mongoose = require('mongoose');
 const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
 
 
 // const session = require('express-session');
@@ -61,7 +62,7 @@ app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 app.use('/imageUpload', uploadRouter);
-
+app.use('favorites', favoriteRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
